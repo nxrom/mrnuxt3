998dd6b85c1dc447c433dc520f8fb59a34ae16a7
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-02-17 09:27:14 -0300

    chore: use tailwind instead naiveui as css library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,4 @@
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-
 export default defineNuxtConfig({
   srcDir: 'src',
-  build: {
-    transpile:
-      process.env.NODE_ENV === 'production'
-        ? [
-            'naive-ui',
-            'vueuc',
-            '@css-render/vue3-ssr',
-            '@juggle/resize-observer'
-          ]
-        : ['@juggle/resize-observer']
-  },
-  vite: {
-    plugins: [
-      Components({
-        resolvers: [NaiveUiResolver()]
-      })
-    ],
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-          : []
-    }
-  }
+  modules: ['@nuxtjs/tailwindcss']
 })
