036fc9bdf1aed0ff47cdbfce2f4021041ac3c173
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:16:51 -0300

    feat: add button component

---
+++ src/components/VButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <component
+    :is="tag"
+    :class="{
+      'button--primary': color === 'primary',
+      'button--secondary': color === 'secondary',
+    }"
+    class="button"
+    v-bind="$attrs"
+  >
+    <span>
+      <slot />
+    </span>
+    <Icon v-if="icon" :name="icon" class="w-5 h-5" />
+  </component>
+</template>
+
+<script setup>
+defineProps({
+  tag: {
+    type: String,
+    default: 'button'
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  },
+  icon: {
+    type: String,
+    default: null
+  }
+})
+</script>
