39376ea57481669e57087b865e9b8bbdb56fabe2
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:08:18 -0300

    feat: add default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="relative min-h-screen bg-neutral-900">
+    <VHeader />
+    <main class="flex flex-col container mx-auto w-full min-h-[calc(100vh-4rem)]">
+      <slot />
+    </main>
+  </div>
+</template>
