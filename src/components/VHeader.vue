bcfda61f426a5dfa8a8faa8a2a9fb1b7d3b4c7eb
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:07:00 -0300

    feat: add header component

---
+++ src/components/VHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="h-16 bg-neutral-800">
+    <div class="container mx-auto flex w-full h-full justify-between px-5 sm:px-0">
+      <div class="flex items-center gap-x-2">
+        <Icon name="logos:nuxt-icon" class="w-8 h-8" />
+        <strong class="cursor-default text-xl text-white font-bold">
+          Nuxt Academy
+        </strong>
+      </div>
+    </div>
+  </header>
+</template>
