ef485edafd2ed6940dff3e6336ad4a91c4492447
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:19:21 -0300

    feat: dynamic lesson page

---
+++ src/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="w-full px-5 overflow-hidden xl:w-[calc(100%-30rem)] sm:px-0">
+    <div class="relative rounded" style="padding: 28.25%; overflow: hidden; max-height: 452px;">
+      <template v-if="lesson?.title && lesson?.videoId">
+        <VPlayer :title="lesson.title" :video-id="lesson.videoId" />
+      </template>
+    </div>
+    <div class="flex flex-col mt-8 mb-4">
+      <div class="flex flex-1 mb-4 sm:justify-end">
+        <ClientOnly>
+          <VLessonCompleteButton
+            v-model:completed="isLessonCompleted"
+            class="w-full sm:w-auto"
+            @update:completed="handleComplete"
+          />
+        </ClientOnly>
+      </div>
+      <hgroup>
+        <p class="text-emerald-500 uppercase text-xs font-medium mb-1">
+          {{ course.title }}
+        </p>
+        <h1 class="text-lg font-bold text-white lg:text-2xl xl:text-3xl">
+          {{ lesson?.title }}
+        </h1>
+      </hgroup>
+      <p class="py-3 text-sm text-neutral-400 xl:text-base">
+        {{ lesson?.text }}
+      </p>
+    </div>
+    <div class="flex flex-col gap-4 sm:flex-row">
+      <VButton
+        v-if="lesson?.sourceUrl"
+        :href="lesson.sourceUrl"
+        tag="a"
+        color="secondary"
+      >
+        Download source code
+      </VButton>
+      <VButton
+        v-if="lesson?.downloadUrl"
+        :href="lesson.downloadUrl"
+        tag="a"
+        color="secondary"
+        icon="heroicons:arrow-down-tray"
+      >
+        Download video
+      </VButton>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const course = useCourse()
+
+const progress = useLocalStorage('progress', [])
+
+const chapter = computed(() => {
+  return course.chapters.find(chapter => chapter.slug === route.params.chapterSlug)
+})
+
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(lesson => lesson.slug === route.params.lessonSlug)
+})
+
+const title = computed(() => {
+  return lesson.value?.title ? `${lesson.value.title} | ${course.title}` : course.title
+})
+
+const description = computed(() => lesson.value?.text)
+
+const isLessonCompleted = computed(() => {
+  if (!progress.value[chapter.value?.number - 1]) {
+    return false
+  }
+
+  if (!progress.value[chapter.value?.number - 1][lesson.value?.number - 1]) {
+    return false
+  }
+
+  return progress.value[chapter.value?.number - 1][lesson.value?.number - 1]
+})
+
+function handleComplete () {
+  if (!progress.value[chapter.value?.number - 1]) {
+    progress.value[chapter.value?.number - 1] = []
+  }
+
+  progress.value[chapter.value?.number - 1][lesson.value?.number - 1] = true
+}
+
+useHead({
+  title,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: description.value
+    }
+  ]
+})
+</script>
