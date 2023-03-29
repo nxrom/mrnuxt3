d94c15bdc76ffb30cf0fb857b9f9bf89a7c61e6b
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:18:10 -0300

    feat: add button to mark lesson as completed

---
+++ src/components/VLessonCompleteButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <label
+    class="button"
+    :class="{
+      'button--primary': completed,
+      'button--tertiary': !completed,
+    }"
+  >
+    <input
+      type="checkbox"
+      class="hidden"
+      :value="completed"
+      @input="emit('update:completed')"
+    >
+    <template v-if="completed">
+      Completed <Icon name="heroicons:check-circle" class="w-5 h-5" />
+    </template>
+    <template v-else>
+      Mark as completed
+    </template>
+  </label>
+</template>
+
+<script setup>
+defineProps({
+  completed: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const emit = defineEmits(['update:completed'])
+</script>
