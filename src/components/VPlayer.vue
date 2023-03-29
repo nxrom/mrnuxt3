921fa1d00de8f661c77393261619a618f376f3eb
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:17:14 -0300

    feat: add video player component

---
+++ src/components/VPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <iframe
+    :src="`https://player.vimeo.com/video/${videoId}`"
+    :title="title"
+    class="absolute inset-0 h-full w-full"
+    allowfullscreen
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    loading="lazy"
+  />
+</template>
+
+<script setup>
+defineProps({
+  videoId: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
