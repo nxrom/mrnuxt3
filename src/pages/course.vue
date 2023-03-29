c50c88a1c61d4a76fea5cd32842138854ba4d0c8
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-29 10:12:40 -0300

    feat: add course page

---
+++ src/pages/course.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-1 flex-wrap my-10">
+    <NuxtPage />
+    <VAside :chapters="chapters" />
+  </div>
+</template>
+
+<script setup>
+const { chapters } = useCourse()
+</script>
