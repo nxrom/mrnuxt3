082b0b79d624fd2d07a6b3a154c04a426884d96b
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-30 15:38:57 -0300

    feat: add login page

---
+++ src/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-[calc(100vh-4rem)] flex flex-col px-8 justify-center">
+    <h1 class="text-2xl text-white font-bold sm:text-4xl">
+      Log in to your account
+    </h1>
+    <p class="text-neutral-400 mt-2 mb-10 sm:text-lg">
+      Welcome back! We're happy to have you here.
+    </p>
+    <VButton
+      icon="grommet-icons:github"
+      color="github"
+    >
+      Log in with Github
+    </VButton>
+  </div>
+</template>
