a24865ad9546f223e51ab3cbb093c162a38794ec
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-30 15:36:58 -0300

    style: adjust spacing between page components

--- src/pages/course.vue
+++ src/pages/course.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-1 flex-wrap my-10">
+  <div class="flex flex-1 flex-wrap my-10 xl:justify-between">
     <NuxtErrorBoundary>
       <NuxtPage />
     </NuxtErrorBoundary>
