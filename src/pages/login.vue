da200d874949d8439481f13f4c5a4cb269627e30
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-31 18:31:28 -0300

    feat: ensure user can login with github account

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,11 +6,42 @@
     <p class="text-neutral-400 mt-2 mb-10 sm:text-lg">
       Welcome back! We're happy to have you here.
     </p>
-    <VButton
-      icon="grommet-icons:github"
-      color="github"
-    >
-      Log in with Github
-    </VButton>
+    <div>
+      <VButton
+        icon="ph:github-logo"
+        color="github"
+        @click="OAuthGithubLogin"
+      >
+        Log in with Github
+      </VButton>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+const user = useSupabaseUser()
+const supbase = useSupabaseClient()
+
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(route.query.redirectTo as string, {
+      replace: true
+    })
+  }
+})
+
+async function OAuthGithubLogin () {
+  const { error } = await supbase.auth.signInWithOAuth(
+    { provider: 'github' }
+  )
+
+  if (error) {
+    return new Error(error.message)
+  }
+}
+
+useHead({
+  title: 'Log In'
+})
+</script>
