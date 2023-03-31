f323e12fe10bf84bf676045601d5c0598a80b7db
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-31 18:27:33 -0300

    feat: add title template for the application pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: 'Nuxt Academy | %s'
+    }
+  },
   srcDir: 'src',
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@vueuse/nuxt', 'nuxt-icon']
 })
