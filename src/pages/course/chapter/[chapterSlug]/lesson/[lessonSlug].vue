30f9c96aaa20a3560ee846d867c959839a7a52fa
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2023-03-31 15:30:38 -0300

    refactor: move route validation middleware to single file

--- src/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ src/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -54,27 +54,7 @@ const route = useRoute()
 const course = useCourse()
 
 definePageMeta({
-  middleware: function ({ params }) {
-    const course = useCourse()
-
-    const chapter = course.chapters.find(chapter => chapter.slug === params.chapterSlug)
-
-    if (!chapter) {
-      return abortNavigation(createError({
-        statusCode: 404,
-        message: 'Chapter not found'
-      }))
-    }
-
-    const lesson = chapter.lessons.find(lesson => lesson.slug === params.lessonSlug)
-
-    if (!lesson) {
-      return abortNavigation(createError({
-        statusCode: 404,
-        message: 'Lesson not found'
-      }))
-    }
-  }
+  middleware: ['route-validation']
 })
 
 const progress = useLocalStorage('progress', [])
